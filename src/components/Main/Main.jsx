5e6037d3f27de4f2ffe2088d0994129e7228affc
Melodi Balossino <melodibalossino@gmail.com>
2023-12-02 18:54:48 -0300

    Fix-Main-component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import imgMobile from "../../assets/mobile/image-header.jpg"
 import DesktopImg from "../../assets/desktop/image-header.jpg"
+import arrow from "../../assets/icon-arrow-down.svg"
 
 function Main() {
   return (
@@ -9,6 +10,9 @@ function Main() {
         <source media="(min-width:641px)" srcSet={DesktopImg} />
         <img src={imgMobile} alt="Background header" />
       </picture>
+      <p className="absolute font-Fraunces text-[40px] uppercase tracking-[6.25px] text-white text-center w-[327px] top-[144px] left-0 right-0 mx-auto
+      sm:w-[600px] sm:top-[100px] lg:top-[120px] xl:top-[170px] 2xl:top-[320px] 2xl:text-5xl">We are creatives</p>
+      <img className="absolute top-[294px] left-0 right-0 mx-auto sm:top-[180px] lg:top-[220px] xl:top-[300px] 2xl:top-[480px]" src={arrow} alt="" />
     </main>
   )
 }
