f03acea26a9122e9e8b8be1aeb82b256cffc4e0e
Melodi Balossino <melodibalossino@gmail.com>
2023-12-02 20:19:27 -0300

    Component-image

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,19 @@
+import Image from "../Image/Image"
 
+import eggMobile from "../../assets/mobile/image-transform.jpg"
+import eggDesktop from "../../assets/desktop/image-transform.jpg"
+import glassMobile from "../../assets/mobile/image-stand-out.jpg"
+import glassDesktop from "../../assets/desktop/image-stand-out.jpg"
 
 const Services = () => {
   return (
     <section>
-        
+      <Image
+        imgMobile={eggMobile}
+        imgDesktop={eggDesktop} />
+      <Image
+        imgMobile={glassMobile}
+        imgDesktop={glassDesktop} />
     </section>
   )
 }
